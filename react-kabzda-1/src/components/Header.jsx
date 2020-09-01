e5596be4725159717722af710648705d7e70fe0b
Witek <witeku777@gmail.com>
2020-09-01 20:24:30 +0200

    12

---
+++ react-kabzda-1/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+return <header className="header">
+<img src="https://i.dlpng.com/static/png/6998260_preview.png" />
+</header>
+}
+
+export default Header;
---
+++ react-kabzda-1/src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className="content">
+    <div>
+      <a><img src="https://www.imperiumtapet.com/public/uploads/preview/1920x1080-1776815563727220uohpchfys.jpeg" /></a>
+    </div>
+    <div>ava+ds
+    </div>My posts<div>
+      <div>New posts</div>
+      <div>post 1</div>
+      <div>post 2</div>
+    </div>
+    </div>
+
+}
+    export default Profile;

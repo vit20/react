3033f30be4a7da7b7c3b8387f19ad5cddedaa64c
Witek <witeku777@gmail.com>
2020-09-19 14:46:27 +0200

    reducer

--- react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-import {updateNewPostActionCreator,addPostActionCreator} from './../../../redux/state';
+import {updateNewPostActionCreator,addPostActionCreator} from './../../../redux/profile-reducer';
 
 
 
--- react-kabzda-1/src/redux/state.js
+++ react-kabzda-1/src/redux/state.js
@@ -1,85 +1,66 @@
-const ADD_POST = "ADD-POST";
-const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
-const UPDATE_NEW_MESSAGE_BODY = "UPDATE_NEW_MESSAGE_BODY";
-const SEND_MESSAGE = "SEND_MESSAGE";
+import profileReducer from "./profile-reducer";
+import dialogReducer from "./dialogs-reducer";
+import sidebarReducer from "./sidebar-reducer";
 
-let store ={
-  _state : {
-  profilePage: {
-    posts: [
-      { id: 1, message: 'Hi, how ara you?', likesCount: 12 },
-      { id: 2, message: 'It\'s my life', likesCount: 11 },
-      { id: 3, message: 'Blala', likesCount: 12 },
-      { id: 4, message: 'Dada', likesCount: 11 }
-    ],
-    newPostText: '123'
 
+
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: 'Hi, how ara you?', likesCount: 12 },
+        { id: 2, message: 'It\'s my life', likesCount: 11 },
+        { id: 3, message: 'Blala', likesCount: 12 },
+        { id: 4, message: 'Dada', likesCount: 11 }
+      ],
+      newPostText: '123'
+
+    },
+
+    dialogsPage: {
+
+      dialogs: [
+        { id: 1, name: 'Witold' },
+        { id: 2, name: 'Sergio' },
+        { id: 3, name: 'Andre' },
+        { id: 4, name: 'Ivan' },
+        { id: 5, name: 'Alex' },
+        { id: 6, name: 'Den' },
+      ],
+
+      messages: [
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'How is yor?' },
+        { id: 3, message: 'Yo' },
+        { id: 4, message: 'Yo' },
+        { id: 5, message: 'Yo' },
+      ],
+      newMessageBody: ""
+    },
+    sidebar: {
+
+    }
   },
-
-  dialogsPage: {
-
-    dialogs: [
-      { id: 1, name: 'Witold' },
-      { id: 2, name: 'Sergio' },
-      { id: 3, name: 'Andre' },
-      { id: 4, name: 'Ivan' },
-      { id: 5, name: 'Alex' },
-      { id: 6, name: 'Den' },
-    ],
-
-    messages: [
-      { id: 1, message: 'Hi' },
-      { id: 2, message: 'How is yor?' },
-      { id: 3, message: 'Yo' },
-      { id: 4, message: 'Yo' },
-      { id: 5, message: 'Yo' },
-    ],
-    newMessageBody:""
+  _callSubscriber() {
+    console.log('State changet');
   },
-  sidebar: {}
-},
-_callSubscriber() {
-  console.log('State changet');
-},
-getState () {
-  return  this._state;
-},
-subscribe (observer) {
-  this._callSubscriber = observer;
-},
-dispatch (action) {
+  getState() {
+    return this._state;
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
+  dispatch(action) {
 
-if(action.type ===ADD_POST){
-  let newPost = {
-    id: 5,
-    message: this._state.profilePage.newPostText,
-    likesCount: 0
-  };
-  this._state.profilePage.posts.push(newPost);
-  this._state.profilePage.newPostText = '';
- this._callSubscriber(this._state);
-} else if (action.type === UPDATE_NEW_POST_TEXT){
-  this._state.profilePage.newPostText = action.newTex;
-  this._callSubscriber(this._state);
-}
-else if (action.type === UPDATE_NEW_MESSAGE_BODY){
-  this._state.dialogsPage.newMessageBody = action.body;
-  this._callSubscriber(this._state);
-}
-else if (action.type === SEND_MESSAGE){
-  let body =this._state.dialogsPage.newMessageBody;
-  this._state.dialogsPage.newMessageBody = '';
-  this._state.dialogsPage.messages.push({id: 6, message: body});
-  this._callSubscriber(this._state);
-}
-}
-}
-export const addPostActionCreator = () => ({type: ADD_POST})
-export const updateNewPostActionCreator = (text) =>({type: UPDATE_NEW_POST_TEXT, newText: text})
+    this._state.profilePage = profileReducer(this._state.profilePage, action);
+    this._state.dialogsPage = dialogReducer(this._state.dialogsPage, action);
+    this._state.sidebar = sidebarReducer(this._state.sidebar, action);
+    this._callSubscriber(this._state);
+  }
 
-export const sendMessageCreator = () => ({type: SEND_MESSAGE})
-export const updateNewMessageBodyCreator = (body) =>({type: UPDATE_NEW_MESSAGE_BODY, body:body})
-  
+}
+
+
 
 export default store;
 window.store = store;

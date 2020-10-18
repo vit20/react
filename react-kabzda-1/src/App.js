dcfcdf1e0d2356b18943fac300f30ef7855939b4
Witek <witeku777@gmail.com>
2020-10-18 19:02:43 +0200

    finitto

--- react-kabzda-1/src/App.js
+++ react-kabzda-1/src/App.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {Route, withRouter} from "react-router-dom";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import {BrowserRouter, Route, withRouter} from "react-router-dom";
+
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
-import {connect} from "react-redux";
+import {connect, Provider} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
+import store from "./redux/redux-store";
+import {withSuspense} from "./hoc/withSuspense";
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+
+
 
 class App extends Component {
     componentDidMount() {
@@ -19,27 +25,27 @@ class App extends Component {
 
     render() {
         if (!this.props.initialized) {
-            return <Preloader/>
+            return <Preloader />
         }
 
         return (
-            <div className='app-wrapper'>
-                <HeaderContainer/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Route path='/dialogs'
-                           render={() => <DialogsContainer/>}/>
+                    <div className='app-wrapper'>
+                        <HeaderContainer/>
+                        <Navbar/>
+                        <div className='app-wrapper-content'>
+                            <Route path='/dialogs'
+                                   render={withSuspense(DialogsContainer)}/>
 
-                    <Route path='/profile/:userId?'
-                           render={() => <ProfileContainer/>}/>
+                            <Route path='/profile/:userId?'
+                                   render={withSuspense(ProfileContainer)} />
 
-                    <Route path='/users'
-                           render={() => <UsersContainer/>}/>
+                            <Route path='/users'
+                                   render={() => <UsersContainer/>}/>
 
-                    <Route path='/login'
-                           render={() => <LoginPage/>}/>
-                </div>
-            </div>
+                            <Route path='/login'
+                                   render={() => <LoginPage/>}/>
+                        </div>
+                    </div>
         )
     }
 }
@@ -48,6 +54,16 @@ const mapStateToProps = (state) => ({
     initialized: state.app.initialized
 })
 
-export default compose(
+let AppContainer = compose(
     withRouter,
     connect(mapStateToProps, {initializeApp}))(App);
+
+const SamuraiJSApp = (props) => {
+   return <BrowserRouter>
+        <Provider store={store}>
+            <AppContainer />
+        </Provider>
+    </BrowserRouter>
+}
+
+export default SamuraiJSApp;

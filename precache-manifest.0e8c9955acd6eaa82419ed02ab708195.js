f7a48260c32559892b29aed3d346a8aad47f4b49
Witek <witeku777@gmail.com>
2020-10-22 22:24:30 +0200

    Updates

---
+++ precache-manifest.0e8c9955acd6eaa82419ed02ab708195.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9751288c67664b49486cc7799b9fa25",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "607d5d9798ca08f67abe",
+    "url": "/react/static/css/3.a563791a.chunk.css"
+  },
+  {
+    "revision": "68d5a90d43d62eef28cb",
+    "url": "/react/static/css/4.3fe81b07.chunk.css"
+  },
+  {
+    "revision": "e509dd6db56941c67cd2",
+    "url": "/react/static/css/main.e1eeed14.chunk.css"
+  },
+  {
+    "revision": "b3c13dd8e6eb03b0c2de",
+    "url": "/react/static/js/2.0f797f0f.chunk.js"
+  },
+  {
+    "revision": "607d5d9798ca08f67abe",
+    "url": "/react/static/js/3.d1a3a6c6.chunk.js"
+  },
+  {
+    "revision": "68d5a90d43d62eef28cb",
+    "url": "/react/static/js/4.851f0c61.chunk.js"
+  },
+  {
+    "revision": "e509dd6db56941c67cd2",
+    "url": "/react/static/js/main.edc20bac.chunk.js"
+  },
+  {
+    "revision": "96e1b01b7e804f018b5a",
+    "url": "/react/static/js/runtime~main.0faed352.js"
+  },
+  {
+    "revision": "6e7c9db7e1f100edac7c6e48714d2ea0",
+    "url": "/react/static/media/preloader.6e7c9db7.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/react/static/media/user.8b1dbb1f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-way-of-samurai/precache-manifest.bd782d70bfaaafd74142cbcebcde7edf.js"
+  "/react/precache-manifest.0e8c9955acd6eaa82419ed02ab708195.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-way-of-samurai/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
